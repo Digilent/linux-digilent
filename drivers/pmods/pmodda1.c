ef285f0dff30ccd7c9f71e462dfa0c122121d835
Tinghui WANG (Steven) <steven.wang@digilentinc.com>
2012-12-18 12:15:47 -0800

    PMOD: GPL License Header Added

--- drivers/pmods/pmodda1.c
+++ drivers/pmods/pmodda1.c
@@ -1,6 +1,24 @@
 /*
- * Digilent PmodDA1 linux driver for Zed
+ * pmodda1.c - Digilent PmodDA1 driver
+ *
+ * Copyright (c) 2012 Digilent.  All right reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *    
  */
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
