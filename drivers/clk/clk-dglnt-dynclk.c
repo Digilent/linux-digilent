47571a0fc5bb7769a68fddbdd59b61ac3d0b2eb8
Jason Wu <jason.wu.misc@gmail.com>
2016-03-03 16:08:19 +1000

    clk: clk-dglnt-dynclk: minor coding style clean up
    and replace space with tab
    
    Signed-off-by: Jason Wu <jason.wu.misc@gmail.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- drivers/clk/clk-dglnt-dynclk.c
+++ drivers/clk/clk-dglnt-dynclk.c
@@ -35,11 +35,13 @@
 #define CLK_BIT_WEDGE 13
 #define CLK_BIT_NOCOUNT 12
 
-#define ERR_CLKCOUNTCALC 0xFFFFFFFF //This value is used to signal an error
+/* This value is used to signal an error */
+#define ERR_CLKCOUNTCALC 0xFFFFFFFF
 #define ERR_CLKDIVIDER (1 << CLK_BIT_WEDGE | 1 << CLK_BIT_NOCOUNT)
 
 #define DYNCLK_DIV_1_REGMASK 0x1041
-#define DYNCLK_DEFAULT_FREQ 125000 //25 MHz (125 KHz / 5)
+/* 25 MHz (125 KHz / 5) */
+#define DYNCLK_DEFAULT_FREQ 125000
 
 #define MMCM_FREQ_VCOMIN 600000
 #define MMCM_FREQ_VCOMAX 1200000
@@ -63,137 +65,137 @@
 #define OFST_DISPLAY_FLTR_LOCK_H 0x1C
 
 static const u64 lock_lookup[64] = {
-   0b0011000110111110100011111010010000000001,
-   0b0011000110111110100011111010010000000001,
-   0b0100001000111110100011111010010000000001,
-   0b0101101011111110100011111010010000000001,
-   0b0111001110111110100011111010010000000001,
-   0b1000110001111110100011111010010000000001,
-   0b1001110011111110100011111010010000000001,
-   0b1011010110111110100011111010010000000001,
-   0b1100111001111110100011111010010000000001,
-   0b1110011100111110100011111010010000000001,
-   0b1111111111111000010011111010010000000001,
-   0b1111111111110011100111111010010000000001,
-   0b1111111111101110111011111010010000000001,
-   0b1111111111101011110011111010010000000001,
-   0b1111111111101000101011111010010000000001,
-   0b1111111111100111000111111010010000000001,
-   0b1111111111100011111111111010010000000001,
-   0b1111111111100010011011111010010000000001,
-   0b1111111111100000110111111010010000000001,
-   0b1111111111011111010011111010010000000001,
-   0b1111111111011101101111111010010000000001,
-   0b1111111111011100001011111010010000000001,
-   0b1111111111011010100111111010010000000001,
-   0b1111111111011001000011111010010000000001,
-   0b1111111111011001000011111010010000000001,
-   0b1111111111010111011111111010010000000001,
-   0b1111111111010101111011111010010000000001,
-   0b1111111111010101111011111010010000000001,
-   0b1111111111010100010111111010010000000001,
-   0b1111111111010100010111111010010000000001,
-   0b1111111111010010110011111010010000000001,
-   0b1111111111010010110011111010010000000001,
-   0b1111111111010010110011111010010000000001,
-   0b1111111111010001001111111010010000000001,
-   0b1111111111010001001111111010010000000001,
-   0b1111111111010001001111111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001,
-   0b1111111111001111101011111010010000000001
+	0b0011000110111110100011111010010000000001,
+	0b0011000110111110100011111010010000000001,
+	0b0100001000111110100011111010010000000001,
+	0b0101101011111110100011111010010000000001,
+	0b0111001110111110100011111010010000000001,
+	0b1000110001111110100011111010010000000001,
+	0b1001110011111110100011111010010000000001,
+	0b1011010110111110100011111010010000000001,
+	0b1100111001111110100011111010010000000001,
+	0b1110011100111110100011111010010000000001,
+	0b1111111111111000010011111010010000000001,
+	0b1111111111110011100111111010010000000001,
+	0b1111111111101110111011111010010000000001,
+	0b1111111111101011110011111010010000000001,
+	0b1111111111101000101011111010010000000001,
+	0b1111111111100111000111111010010000000001,
+	0b1111111111100011111111111010010000000001,
+	0b1111111111100010011011111010010000000001,
+	0b1111111111100000110111111010010000000001,
+	0b1111111111011111010011111010010000000001,
+	0b1111111111011101101111111010010000000001,
+	0b1111111111011100001011111010010000000001,
+	0b1111111111011010100111111010010000000001,
+	0b1111111111011001000011111010010000000001,
+	0b1111111111011001000011111010010000000001,
+	0b1111111111010111011111111010010000000001,
+	0b1111111111010101111011111010010000000001,
+	0b1111111111010101111011111010010000000001,
+	0b1111111111010100010111111010010000000001,
+	0b1111111111010100010111111010010000000001,
+	0b1111111111010010110011111010010000000001,
+	0b1111111111010010110011111010010000000001,
+	0b1111111111010010110011111010010000000001,
+	0b1111111111010001001111111010010000000001,
+	0b1111111111010001001111111010010000000001,
+	0b1111111111010001001111111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001,
+	0b1111111111001111101011111010010000000001
 };
 
 static const u32 filter_lookup_low[64] = {
-	 0b0001011111,
-	 0b0001010111,
-	 0b0001111011,
-	 0b0001011011,
-	 0b0001101011,
-	 0b0001110011,
-	 0b0001110011,
-	 0b0001110011,
-	 0b0001110011,
-	 0b0001001011,
-	 0b0001001011,
-	 0b0001001011,
-	 0b0010110011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001010011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0001100011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010010011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011,
-	 0b0010100011
+	0b0001011111,
+	0b0001010111,
+	0b0001111011,
+	0b0001011011,
+	0b0001101011,
+	0b0001110011,
+	0b0001110011,
+	0b0001110011,
+	0b0001110011,
+	0b0001001011,
+	0b0001001011,
+	0b0001001011,
+	0b0010110011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001010011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0001100011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010010011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011,
+	0b0010100011
 };
 
 struct dglnt_dynclk_reg;
@@ -201,25 +203,25 @@ struct dglnt_dynclk_mode;
 struct dglnt_dynclk;
 
 struct dglnt_dynclk_reg{
-		u32 clk0L;
-		u32 clkFBL;
-		u32 clkFBH_clk0H;
-		u32 divclk;
-		u32 lockL;
-		u32 fltr_lockH;
+	u32 clk0L;
+	u32 clkFBL;
+	u32 clkFBH_clk0H;
+	u32 divclk;
+	u32 lockL;
+	u32 fltr_lockH;
 };
 
 struct dglnt_dynclk_mode{
-		u32 freq;
-		u32 fbmult;
-		u32 clkdiv;
-		u32 maindiv;
+	u32 freq;
+	u32 fbmult;
+	u32 clkdiv;
+	u32 maindiv;
 };
 
 struct dglnt_dynclk {
 	void __iomem *base;
 	struct clk_hw clk_hw;
-   unsigned long freq;
+	unsigned long freq;
 };
 
 u32 dglnt_dynclk_divider(u32 divide)
@@ -235,13 +237,11 @@ u32 dglnt_dynclk_divider(u32 divide)
 		return DYNCLK_DIV_1_REGMASK;
 
 	highTime = divide / 2;
-	if (divide & 0b1) //if divide is odd
-	{
+	/* if divide is odd */
+	if (divide & 0b1) {
 		lowTime = highTime + 1;
 		output = 1 << CLK_BIT_WEDGE;
-	}
-	else
-	{
+	} else {
 		lowTime = highTime;
 	}
 
@@ -293,14 +293,13 @@ int dglnt_dynclk_find_reg (struct dglnt_dynclk_reg *regValues, struct dglnt_dync
 
 void dglnt_dynclk_write_reg (struct dglnt_dynclk_reg *regValues, void __iomem *baseaddr)
 {
-   writel(regValues->clk0L, baseaddr + OFST_DISPLAY_CLK_L);
-   writel(regValues->clkFBL, baseaddr + OFST_DISPLAY_FB_L);
-   writel(regValues->clkFBH_clk0H, baseaddr + OFST_DISPLAY_FB_H_CLK_H);
-   writel(regValues->divclk, baseaddr + OFST_DISPLAY_DIV);
-   writel(regValues->lockL, baseaddr + OFST_DISPLAY_LOCK_L);
-   writel(regValues->fltr_lockH, baseaddr + OFST_DISPLAY_FLTR_LOCK_H);
+	writel(regValues->clk0L, baseaddr + OFST_DISPLAY_CLK_L);
+	writel(regValues->clkFBL, baseaddr + OFST_DISPLAY_FB_L);
+	writel(regValues->clkFBH_clk0H, baseaddr + OFST_DISPLAY_FB_H_CLK_H);
+	writel(regValues->divclk, baseaddr + OFST_DISPLAY_DIV);
+	writel(regValues->lockL, baseaddr + OFST_DISPLAY_LOCK_L);
+	writel(regValues->fltr_lockH, baseaddr + OFST_DISPLAY_FLTR_LOCK_H);
 }
-
 
 u32 dglnt_dynclk_find_mode(u32 freq, u32 parentFreq, struct dglnt_dynclk_mode *bestPick)
 {
@@ -308,31 +307,31 @@ u32 dglnt_dynclk_find_mode(u32 freq, u32 parentFreq, struct dglnt_dynclk_mode *b
 	u32 curError;
 	u32 curClkMult;
 	u32 curFreq;
-   u32 divVal;
+	u32 divVal;
 	u32 curFb, curClkDiv;
 	u32 minFb = 0;
 	u32 maxFb = 0;
-   u32 curDiv = 1;
-   u32 maxDiv;
-   bool freq_found = false;
+	u32 curDiv = 1;
+	u32 maxDiv;
+	bool freq_found = false;
 
 	bestPick->freq = 0;
-   if (parentFreq == 0)
-      return 0; 
+	if (parentFreq == 0)
+		return 0;
 
-   if (freq < MMCM_FREQ_OUTMIN)//minimum frequency is actually dictated by VCOmin
-      freq = MMCM_FREQ_OUTMIN;
-   if (freq > MMCM_FREQ_OUTMAX)
-      freq = MMCM_FREQ_OUTMAX;
+	/* minimum frequency is actually dictated by VCOmin */
+	if (freq < MMCM_FREQ_OUTMIN)
+		freq = MMCM_FREQ_OUTMIN;
+	if (freq > MMCM_FREQ_OUTMAX)
+		freq = MMCM_FREQ_OUTMAX;
 
-   if (parentFreq > MMCM_FREQ_PFDMAX)
-      curDiv = 2;      
-   maxDiv = parentFreq / MMCM_FREQ_PFDMIN;
-   if (maxDiv > MMCM_DIV_MAX)
-      maxDiv = MMCM_DIV_MAX;
+	if (parentFreq > MMCM_FREQ_PFDMAX)
+		curDiv = 2;
+	maxDiv = parentFreq / MMCM_FREQ_PFDMIN;
+	if (maxDiv > MMCM_DIV_MAX)
+		maxDiv = MMCM_DIV_MAX;
 
-	while (curDiv <= maxDiv && !freq_found)
-	{
+	while (curDiv <= maxDiv && !freq_found) {
 		minFb = curDiv * DIV_ROUND_UP(MMCM_FREQ_VCOMIN, parentFreq); 
 		maxFb = curDiv * (MMCM_FREQ_VCOMAX / parentFreq); 
 		if (maxFb > MMCM_FB_MAX)
@@ -340,37 +339,39 @@ u32 dglnt_dynclk_find_mode(u32 freq, u32 parentFreq, struct dglnt_dynclk_mode *b
 		if (minFb < MMCM_FB_MIN)
 			minFb = MMCM_FB_MIN;
 
-      divVal = curDiv * freq;
-      curClkMult = ((parentFreq * 1000) + (divVal / 2)) / divVal; //This multiplier is used to find the best clkDiv value for each FB value
-		
+		divVal = curDiv * freq;
+		/*
+		 * This multiplier is used to find the best clkDiv value for
+		 * each FB value
+		 */
+		curClkMult = ((parentFreq * 1000) + (divVal / 2)) / divVal;
+
 		curFb = minFb;
-	   while (curFb <= maxFb && !freq_found)
-	   {
-		   curClkDiv = ((curClkMult * curFb) + 500) / 1000;
-         if (curClkDiv > MMCM_CLKDIV_MAX)
-             curClkDiv = MMCM_CLKDIV_MAX;
-         if (curClkDiv < MMCM_CLKDIV_MIN)
-            curClkDiv = MMCM_CLKDIV_MIN;
-		   curFreq = (((parentFreq * curFb) / curDiv) / curClkDiv);
-         if (curFreq >= freq)			
-            curError = curFreq - freq;
-         else
-            curError = freq - curFreq;
-		   if (curError < bestError)
-		   {
-			   bestError = curError;
-			   bestPick->clkdiv = curClkDiv;
-			   bestPick->fbmult = curFb;
-			   bestPick->maindiv = curDiv;
-			   bestPick->freq = curFreq;
-		   }
-         if (!curError)
-            freq_found = true;
-		   curFb++;
-      }
-      curDiv++;
+		while (curFb <= maxFb && !freq_found) {
+			curClkDiv = ((curClkMult * curFb) + 500) / 1000;
+			if (curClkDiv > MMCM_CLKDIV_MAX)
+				curClkDiv = MMCM_CLKDIV_MAX;
+			if (curClkDiv < MMCM_CLKDIV_MIN)
+				curClkDiv = MMCM_CLKDIV_MIN;
+			curFreq = (((parentFreq * curFb) / curDiv) / curClkDiv);
+			if (curFreq >= freq)
+				curError = curFreq - freq;
+			else
+				curError = freq - curFreq;
+			if (curError < bestError) {
+				bestError = curError;
+				bestPick->clkdiv = curClkDiv;
+				bestPick->fbmult = curFb;
+				bestPick->maindiv = curDiv;
+				bestPick->freq = curFreq;
+			}
+			if (!curError)
+				freq_found = true;
+			curFb++;
+		}
+		curDiv++;
 	}
-   return bestPick->freq;
+	return bestPick->freq;
 }
 
 static struct dglnt_dynclk *clk_hw_to_dglnt_dynclk(struct clk_hw *clk_hw)
@@ -384,8 +385,7 @@ static int dglnt_dynclk_enable(struct clk_hw *clk_hw)
 	struct dglnt_dynclk *dglnt_dynclk = clk_hw_to_dglnt_dynclk(clk_hw);
 	unsigned int clock_state;
 
-	if (dglnt_dynclk->freq)
-	{
+	if (dglnt_dynclk->freq) {
 		writel(1, dglnt_dynclk->base + OFST_DISPLAY_CTRL);
 		do
 		{
@@ -399,7 +399,7 @@ static void dglnt_dynclk_disable(struct clk_hw *clk_hw)
 {
 	struct dglnt_dynclk *dglnt_dynclk = clk_hw_to_dglnt_dynclk(clk_hw);
 
-   writel(0, dglnt_dynclk->base + OFST_DISPLAY_CTRL);
+	writel(0, dglnt_dynclk->base + OFST_DISPLAY_CTRL);
 }
 
 static int dglnt_dynclk_set_rate(struct clk_hw *clk_hw,
@@ -411,23 +411,29 @@ static int dglnt_dynclk_set_rate(struct clk_hw *clk_hw,
 
 	if (parent_rate == 0 || rate == 0)
 		return -EINVAL;
-   if (rate == dglnt_dynclk->freq)
-      return 0;
-
-	rate = (rate + 100) / 200; //Convert from Hz to KHz, then multiply by five to account for BUFR division
-	parent_rate = (parent_rate + 500) / 1000; //convert from Hz to KHz 
-   if(!dglnt_dynclk_find_mode(rate, parent_rate, &clkMode))
-      return -EINVAL;
+	if (rate == dglnt_dynclk->freq)
+		return 0;
 
 	/*
-	 * Write to the PLL dynamic configuration registers to configure it with the calculated
-	 * parameters.
+	 * Convert from Hz to KHz, then multiply by five to account for
+	 * BUFR division
+	 */
+	rate = (rate + 100) / 200;
+	/* convert from Hz to KHz */
+	parent_rate = (parent_rate + 500) / 1000;
+	if (!dglnt_dynclk_find_mode(rate, parent_rate, &clkMode))
+		return -EINVAL;
+
+	/*
+	 * Write to the PLL dynamic configuration registers to configure it
+	 * with the calculated parameters.
 	 */
 	dglnt_dynclk_find_reg(&clkReg, &clkMode);
 	dglnt_dynclk_write_reg(&clkReg, dglnt_dynclk->base);
-   dglnt_dynclk->freq = clkMode.freq * 200;
-   dglnt_dynclk_disable(clk_hw);
-   dglnt_dynclk_enable(clk_hw);
+	dglnt_dynclk->freq = clkMode.freq * 200;
+	dglnt_dynclk_disable(clk_hw);
+	dglnt_dynclk_enable(clk_hw);
+
 	return 0;
 }
 
@@ -435,8 +441,9 @@ static long dglnt_dynclk_round_rate(struct clk_hw *hw, unsigned long rate,
 	unsigned long *parent_rate)
 {
 	struct dglnt_dynclk_mode clkMode;
-   
-   dglnt_dynclk_find_mode(((rate + 100) / 200), ((*parent_rate) + 500) / 1000, &clkMode);
+
+	dglnt_dynclk_find_mode(((rate + 100) / 200),
+		((*parent_rate) + 500) / 1000, &clkMode);
 
 	return (clkMode.freq * 200);
 }
@@ -445,7 +452,7 @@ static unsigned long dglnt_dynclk_recalc_rate(struct clk_hw *clk_hw,
 	unsigned long parent_rate)
 {
 	struct dglnt_dynclk *dglnt_dynclk = clk_hw_to_dglnt_dynclk(clk_hw);
-	
+
 	return dglnt_dynclk->freq;
 }
 
@@ -503,8 +510,8 @@ static int dglnt_dynclk_probe(struct platform_device *pdev)
 	init.flags = 0;
 	init.parent_names = &parent_name;
 	init.num_parents = 1;
-   
-   dglnt_dynclk->freq = 0;
+
+	dglnt_dynclk->freq = 0;
 	dglnt_dynclk_disable(&dglnt_dynclk->clk_hw);
 
 	dglnt_dynclk->clk_hw.init = &init;
@@ -513,7 +520,7 @@ static int dglnt_dynclk_probe(struct platform_device *pdev)
 		return PTR_ERR(clk);
 
 	return of_clk_add_provider(pdev->dev.of_node, of_clk_src_simple_get,
-				    clk);
+					clk);
 }
 
 static int dglnt_dynclk_remove(struct platform_device *pdev)
